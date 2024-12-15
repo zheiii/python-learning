17053e5d370b59ef8d73ec6a7c207b5c24f3469d
zheiii <mohammadnezhadhanieh@gmail.com>
2024-12-15 02:57:36 -0800

    Update puzzle-1.py

--- puzzle-1.py
+++ puzzle-1.py
@@ -1,4 +1,3 @@
-e = 1
 N =[]
 M =[]
 O =[]
@@ -6,17 +5,15 @@ c = 0
 print("please enter the first group's numbers(type finish at the end:)):")
 while True:
     a = input()
-    c < e
     if a == "finish":
         break
     try:
         a = float(a)
         N.append(a)
         e = len(N)
-        N.sort()
     except ValueError:
         print("invalid input.")
-print("enter the second group's number(type finish at the end:)):")
+print("enter the second group's numbers(type finish at the end:)):")
 while True:
     b = input()
     if b == "finish":
@@ -25,10 +22,12 @@ while True:
         b = float(b)
         M.append(b)
         M.sort()
-        d = M[c]-N[c]
-        O.append(d)
-        c = c + 1
+        N.sort()
     except ValueError:
         print("invalid input.")
+while c < e:
+    d = abs(M[c]-N[c])
+    O.append(d)
+    c = c + 1
 print(sum(O))
 
